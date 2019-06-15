8f4e86635740ed7313d4d71477101d45e0a63cbd
jaididwania <44374541+jaididwania@users.noreply.github.com>
2019-06-15 09:55:17 +0530

    Update script.js

--- module-4-assignment/js/script.js
+++ module-4-assignment/js/script.js
@@ -1,20 +1,26 @@
 
+(function (window) {
+
+
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    for (var i = 0; i < names.length; i++)
-     {
-        var firstLetter = names[i].charAt(0).toLowerCase();
-        if (firstLetter === 'j')
-         {
-            console.log("GoodBye "+names[i])
-        } 
-        else
-         {
-           console.log("Hello "+names[i])
-        }
-    }
 
 
-OUTPUT:
+for (var i in names) {
+
+  
+  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
+
+
+  if (firstLetter == 'j') {
+     window.byeSpeaker.speak(names[i]);
+  } else {
+     window.helloSpeaker.speak(names[i]);
+  }
+}
+})(window);
+
+
+//OUTPUT:
 
 
 //Hello Yaakov
